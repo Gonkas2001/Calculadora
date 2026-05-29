8de463792859894b53910da70c9fcbd1d2764f18
Gonkas2001 <goncalofernandessilva2001@gmail.com>
2026-05-29 11:12:18 +0100

    correção erros
    tinha alguns ":" em falta e as indentactions nao estavam bem feitas

--- main.py
+++ main.py
@@ -31,12 +31,12 @@ def calculadora(num1: float, num2: float, operador: str) -> float:
 def calculadora_v2(num1: float, num2: float, operador: str) -> float:
     
     operacoes = {
-        "+": num1 + num2
-        "-": num1 - num2
-        "*": num1 * num2
-        "/": num1 / num2
-        "**": num1 ** num2
-        "%": num1 % num2
+        "+": num1 + num2,
+        "-": num1 - num2,
+        "*": num1 * num2,
+        "/": num1 / num2,
+        "**": num1 ** num2,
+        "%": num1 % num2,
     }
     
     return operacoes.get(operador, float("nan"))
